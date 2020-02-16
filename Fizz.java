effa2697b0e4d546ccf22c233760cbfa31a50b12
EdZeno <eduard.kulcsickij.gut@gmail.com>
2020-02-16 17:19:46 +0000

    testing java

--- Fizz.java
+++ Fizz.java
@@ -1,11 +1,15 @@
 public class Fizz{
-  private String greatings;
+  // private String greatings;
+  //
+  // public Fizz(String value) {
+  //   greatings = value;
+  // }
 
-  public Fizz(String value) {
-    greatings = value;
-  }
-
+  // public void greater(String argument) {
+  //   System.out.println("argument");
+  // }
   public static void main(String[] args) {
-    Fizz fizz = new Fizz("Hello");
+    System.out.println("Hello");
+    // Fizz fizz = new Fizz("Hello");
   }
 }

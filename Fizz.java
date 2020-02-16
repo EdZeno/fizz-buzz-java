82825545d7a4e4bff1845029995e9c32c4690538
EdZeno <eduard.kulcsickij.gut@gmail.com>
2020-02-16 16:49:30 +0000

    playing with java

--- Fizz.java
+++ Fizz.java
@@ -1,5 +1,11 @@
 public class Fizz{
+  private String greatings;
+
+  public Fizz(String value) {
+    greatings = value;
+  }
+
   public static void main(String[] args) {
-    Fizz fizz = new Fizz();
+    Fizz fizz = new Fizz("Hello");
   }
 }
---
+++ TestTest.java
@@ -0,0 +1,6 @@
+import junit.framework.TestCase;
+public class Test extends TestCase {
+  public void testTrue() {
+    assertTrue(true);
+  }
+}
